792efe6c81a6f9530edae2c1bdd5e8329bcf88a0
kian79 <kiankr79@gmail.com>
2020-04-13 01:40:20 +0430

    Some bugs fixed and made code cleaner.

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
-import os
 import smtplib
-from _stat import S_IWUSR
-
-import numpy as np
 import pandas as pd
+# reading csv file
 students= pd.read_csv('TEST1.csv')
+# getting students' names and adding them to a list
 names=students['نام'].to_list()
 lastnames=students['نام خانوادگی'].to_list()
 for i in range(len(names)):
     names[i]=(names[i]+" "+lastnames[i]).replace('\u200c'," ")
     names[i]=names[i].replace('\u200d',"")
-
 print(names)
+# getting students' names and converting them to a list
 grades = students['نمره'].to_list()
 print(grades)
+# getting students' emails and converting them to a list
 emails = students['ایمیل'].to_list()
 print(emails)
+#getting user's data
 smtp_server = input("Enter your smtp server address")
 gmail_user = input("Enter you email address")
 gmail_password = input("Enter your email password")
-# os.chmod('/usr/lib/python3.6/smtplib.py', S_IWUSR) # This makes the file read/write for the owner
 
 sent_from = gmail_user
+#making server and logging in to user's email
+server = smtplib.SMTP(smtp_server, 587)
+server.ehlo()
+print("1")
+server.starttls()
+print("2")
+server.login(gmail_user, gmail_password)
+print("3")
+#starting to send emails
 for i in range(len(names)):
     to = [emails[i]]
-# for i in range(len(to)):
-    subject = 'midterm grade'
-    body = f' your grade in midterm exam is {grades[i]}. '
-
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
-
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
+#subject of email
     SUBJECT = "نمره تمرین دوم"
+#body text of email
     TEXT=f"""
     با سلام
     دانشجوی عزیز  {names[i]}
@@ -46,18 +45,11 @@ for i in range(len(names)):
     kiankr79@gmail.com
     """
     msg = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT).encode('UTF_8')
-# try:
-    server = smtplib.SMTP(smtp_server, 587)
-    server.ehlo()
-    print("1")
-    server.starttls()
-    print("2")
-    server.login(gmail_user, gmail_password)
-    print("3")
+#
+#sending email to each student
     server.sendmail(sent_from, to[0], msg)
     print("4")
-    server.close()
 
-    print('Email sent!')
-# except:
-    print('failed!')
+    print(f'Email number {i+1} sent!')
+#     print('failed!')
+server.close()
