1b1c9b417b8e4a599b279b46bf260010f26b9de5
Parham Alvani <parham.alvani@gmail.com>
2020-04-19 06:46:08 +0000

    feat: Complete Main

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import smtplib
 import click
-import pandas as pd
+import csv
 from jinja2 import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from typing import List
 
 import config
+import model
 
 # getting information from csv file and returning them as lists.
 
+FIRST_NAME_COLUMN = "نام"
+LAST_NAME_COLUMN = "نام خانوادگی"
+STUDENT_ID_COLUMN = "شماره دانشجویی"
+EMAIL_COLUMN = "ایمیل"
+SKIPPED_ROWS = 2
+# please note that other columns consider as grades
 
-def get_info_from_csv(csv_file):
-    students = pd.read_csv(csv_file)  # reading csv file
-    student_names = students[
-        "نام"
-    ].to_list()  # getting students' names and adding them to a list.
-    lastname = students["نام خانوادگی"].to_list()  # getting students' lastnames.
-    for i in range(len(student_names)):
-        student_names[i] = student_names[i] + " " + lastname[i]
-    student_grades = students["نمره"].to_list()  # getting grades from csv
-    email = students[
-        "ایمیل"
-    ].to_list()  # getting students' emails and converting them to a list
-    return student_names, student_grades, email
+
+def get_info_from_csv(file) -> List[model.Student]:
+    students = []
+    c = 0
+
+    reader = csv.DictReader(file)
+    for row in reader:
+        if c < SKIPPED_ROWS:
+            c += 1
+            continue
+
+        row.pop(STUDENT_ID_COLUMN)
+        name = row.pop(FIRST_NAME_COLUMN) + " " + row.pop(LAST_NAME_COLUMN)
+        email = row.pop(EMAIL_COLUMN)
+        students.append(model.Student(name, email, row))
+
+    return students
 
 
 def run_server(address, username, password):
@@ -66,7 +78,7 @@ def main(input, body, subject):
     cfg = config.load()
 
     # getting data from our csv file
-    names, grades, emails = get_info_from_csv(input)
+    students = get_info_from_csv(input)
 
     # making server
     mail_server = run_server(cfg.email.server, cfg.email.username, cfg.email.password)
@@ -77,8 +89,8 @@ def main(input, body, subject):
     # sending emails to each student one by one.
     body = body.read()
 
-    for i in range(len(names)):
-        to = emails[i]
+    for student in students:
+        to = student.email
 
         message = MIMEMultipart("alternative")
         message["Subject"] = f"{cfg.course.name} - {cfg.course.semester}: {subject}"
@@ -87,13 +99,13 @@ def main(input, body, subject):
 
         # Text of email
         tmpl = Template(body)
-        body = tmpl.render(name=names[i], grade=grades[i])
+        body = tmpl.render(name=student.name, grades=student.grades)
 
         message.attach(MIMEText(body, "html"))
 
         # sending email to each student
         mail_server.sendmail(cfg.email.username, to, message.as_string())
-        print(f"Email number {i + 1} sent to {names[i]}!")
+        print(f"Email sent to {student.name}!")
 
     mail_server.close()
     print("Finished.")
