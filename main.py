dd6d593af90ef7a09fcdd054d64ebaf12661e4bc
Parham Alvani <parham.alvani@gmail.com>
2020-04-19 04:06:30 +0000

    feat: Add Input Flag

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import smtplib
+import click
 import pandas as pd
-import yaml
 from jinja2 import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import config
 
 # getting information from csv file and returning them as lists.
 
@@ -16,8 +17,7 @@ def get_info_from_csv(csv_file):
     ].to_list()  # getting students' names and adding them to a list.
     lastname = students["نام خانوادگی"].to_list()  # getting students' lastnames.
     for i in range(len(student_names)):
-        student_names[i] = (student_names[i] + " " + lastname[i]).replace("\u200c", " ")
-        student_names[i] = student_names[i].replace("\u200d", "")
+        student_names[i] = student_names[i] + " " + lastname[i]
     student_grades = students["نمره"].to_list()  # getting grades from csv
     email = students[
         "ایمیل"
@@ -25,36 +25,38 @@ def get_info_from_csv(csv_file):
     return student_names, student_grades, email
 
 
-# running smtp server and logging in to user's email account.
-def run_server(smtp_address, id, password):
-    smtp = smtplib.SMTP(smtp_address, 587)
+def run_server(address, username, password):
+    """
+    running smtp server and logging in to user's email account.
+    """
+
+    smtp = smtplib.SMTP(address, 587)
     smtp.ehlo()
     smtp.starttls()
     print("Server started.")
-    smtp.login(id, password)
-    print("Logged in to {user}".format(user=id))
+    smtp.login(username, password)
+    print(f"Logged in to {address} with {username}")
     return smtp
 
 
-def login(config_file):
-    with open(config_file, "r") as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.Loader)
-    smtp_address = cfg["configs"]["smtp_address"]
-    email_id = cfg["configs"]["email_address"]
-    email_passwd = cfg["configs"]["password"]
-    return smtp_address, email_id, email_passwd
-
-
-if __name__ == "__main__":
+@click.command()
+@click.option(
+    "--input",
+    "-i",
+    default="sample.csv",
+    help="path to input csv file",
+    type=click.File(mode="r"),
+)
+def main(input):
     # getting user's data and logging in to user's email
-    smtp_server, email_user, email_password = login("config.yml")
+    cfg = config.load()
 
-    sent_from = email_user
     # getting data from our csv file
-    names, grades, emails = get_info_from_csv("sample.csv")
+    print(f"Read information from {input}")
+    names, grades, emails = get_info_from_csv(input)
 
     # making server
-    server = run_server(smtp_server, email_user, email_password)
+    mail_server = run_server(cfg.email.server, cfg.email.username, cfg.email.password)
 
     # reading subject of emails from txt file.
     with open("subject.txt") as subject_file:
@@ -68,8 +70,8 @@ if __name__ == "__main__":
         to = emails[i]
 
         message = MIMEMultipart("alternative")
-        message["Subject"] = subject
-        # message["From"] =
+        message["Subject"] = f"{cfg.course.name} - {cfg.course.semester}: {subject}"
+        message["From"] = cfg.email.username
         message["To"] = to
 
         # Text of email
@@ -79,7 +81,12 @@ if __name__ == "__main__":
         message.attach(MIMEText(body, "html"))
 
         # sending email to each student
-        server.sendmail(sent_from, to, message.as_string())
-        print(f"Email number {i + 1} sent!")
-    server.close()
+        mail_server.sendmail(cfg.email.username, to, message.as_string())
+        print(f"Email number {i + 1} sent to {names[i]}!")
+
+    mail_server.close()
     print("Finished.")
+
+
+if __name__ == "__main__":
+    main()
